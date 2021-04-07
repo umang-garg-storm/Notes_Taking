7aa61f3d143382680c021e3ef08b786a4ea41c26
umang-garg-storm <60745072+umang-garg-storm@users.noreply.github.com>
2021-04-07 23:00:15 +0530

    Assigned id To Card View and XML resource for CardView and Set Card view for setCardBackground color Created custom Method for CardView to select random color from colors.xml Private int getRandomColor()

--- app/src/main/java/com/example/notestaking/model/Adapter.java
+++ app/src/main/java/com/example/notestaking/model/Adapter.java
@@ -7,11 +7,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.notestaking.R;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     List<String> titles;
@@ -34,7 +37,10 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
         // bind actual data recived from main Activity and bind it to the views assigned down wards
          holder.noteTitle.setText(titles.get(position));
          holder.noteContent.setText(content.get(position));
-         // to display message for clicking particular views in recyclerview
+         // To set Background Color for CardView
+        holder.mCardView.setCardBackgroundColor(holder.view.getResources().getColor(getRandomColor(),null));
+        // to get Random color Automatic
+        // to display message for clicking particular views in recyclerview
         holder.view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,7 +48,24 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
             }
         });
 
+    }
+    private int getRandomColor() {
+        // Method for getting Random Color code form colors.xml
+        List<Integer> colorCode = new ArrayList<>();
+        colorCode.add(R.color.purple_200);
+        colorCode.add(R.color.purple_500);
+        colorCode.add(R.color.purple_700);
+        colorCode.add(R.color.teal_200);
+        colorCode.add(R.color.teal_700);
+        colorCode.add(R.color.white);
+        colorCode.add(R.color.red);
+        colorCode.add(R.color.dark_Purple);
+        colorCode.add(R.color.light_pink);
+        colorCode.add(R.color.does_not);
 
+        Random randomColor = new Random(); // Created Random Class for Random Selection of Colors....//
+        int number = randomColor.nextInt(colorCode.size());
+        return colorCode.get(number);
     }
 
     @Override
@@ -54,6 +77,8 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
         // variable created for views(note view layout)
         TextView noteTitle,noteContent;
         View view;
+        //Created Variable for CardVIew
+        CardView mCardView;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -61,6 +86,8 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
             noteTitle = itemView.findViewById(R.id.titles);
             noteContent = itemView.findViewById(R.id.content);
             view = itemView;
+            //assigning XML Resources for CardView
+            mCardView = itemView.findViewById(R.id.noteCard);
 
         }
     }
