a156ac65e948f49d67fdfdc1455f71787bfe1150
umang-garg-storm <60745072+umang-garg-storm@users.noreply.github.com>
2021-05-21 21:56:56 +0530

    added back button

--- app/src/main/java/com/example/notestaking/NoteDetailsActivity.java
+++ app/src/main/java/com/example/notestaking/NoteDetailsActivity.java
@@ -1,8 +1,14 @@
 package com.example.notestaking;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class NoteDetailsActivity extends AppCompatActivity {
 
@@ -10,5 +16,21 @@ public class NoteDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_details);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener((view) -> {
+            Snackbar.make(view, "Replace with your own action,", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
